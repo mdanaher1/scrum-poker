2592ba5943e3bd37ba1f64b3d2e293dcf6e34560
Maurice Danaher <mauricedanaher@live.ie>
2017-06-16 15:17:10 +0100

    - Messing with fragments - Try designing UI with ConstraintLayout - Main/Card fragments, single activity - Remove actionbar

--- app/src/main/java/xyz/mauriced/scrumpoker/MainActivity.java
+++ app/src/main/java/xyz/mauriced/scrumpoker/MainActivity.java
@@ -1,13 +1,101 @@
 package xyz.mauriced.scrumpoker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // The following are used for the shake detection
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                /*
+                 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.
+				 */
+                handleShakeEvent(count);
+            }
+        });
+
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.main_activity, new MainFragment());
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
+
+    }
+
+    public void handleShakeEvent(int count) {
+        Toast.makeText(this, "SHAKE IT OFF!!!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button0:
+                Toast.makeText(getApplicationContext(), "asdfsadf", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button1:
+                // Begin the transaction
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                // Replace the contents of the container with the new fragment
+                ft.replace(R.id.main_activity, new CardFragment())
+                        .addToBackStack("main")
+                        .commit();
+                break;
+            case R.id.button2:
+                break;
+            case R.id.button3:
+                break;
+            case R.id.button5:
+                break;
+            case R.id.button8:
+                break;
+            case R.id.buttoncoffee:
+                break;
+            case R.id.buttonq:
+                break;
+            case R.id.buttonqqq:
+                break;
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 }
